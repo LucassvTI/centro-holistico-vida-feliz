de0ec70f7aaf92d8d9c66d5a3725d486b07faf51
Lucas_viegas <lucas_viegas@sicredi.com.br>
2019-08-21 18:57:49 -0300

    Finalizando formulario de contato

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,11 @@
 'use strict';
 const functions  = require('firebase-functions');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
+const cors = require('cors')({origin: true });
 
 const Credentials = require('./credentials.json');
 
-const whitelist = ['centroholistico-17a4a.web.app', 'centroholistico-17a4a.firebaseapp.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      // eslint-disable-next-line callback-return
-      callback(null, true)
-    } else {
-      // eslint-disable-next-line callback-return
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-exports.enviarEmail = functions.https.onRequest( cors(corsOptions), (req, res) => {
+exports.enviarEmail = functions.https.onRequest((req, res) => {
   // eslint-disable-next-line consistent-return
   cors(req, res, () => {
     if( req.method !== "POST" ){
@@ -30,8 +17,8 @@ exports.enviarEmail = functions.https.onRequest( cors(corsOptions), (req, res) =
     let transporter = nodemailer.createTransport(Credentials);
     
     let email = {
-      from: req.body['remetente'],
-      to: req.body['destinatarios'],
+      from: "Centro Holistico Vida Feliz <chvidafeliz@gmail.com>",
+      to: req.body['destinatarios'].concat(",leandrotviegas@gmail.com"),
       subject: req.body['assunto'],
       text: req.body['corpo'],
     };
